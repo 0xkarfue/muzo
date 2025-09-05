9bf1eb8db69a69c4dc21a92626af6f7b7b298c18
0xkarfue <0xkarfue@gmail.com>
2025-09-05 18:27:02 +0530

    fixed redirect

--- my-app/app/api/auth/[...nextauth]/route.ts
+++ my-app/app/api/auth/[...nextauth]/route.ts
@@ -31,32 +31,3 @@ const handler = NextAuth({
 })
 
 export { handler as GET, handler as POST }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,10 +1,19 @@
 import LandingPage from "@/components/Landing";
+import { getServerSession } from "next-auth";
 import Image from "next/image";
+// import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
+export default async function Home() {
 
-export default function Home() {
+  const session = await getServerSession()
+
+  if (session?.user?.email) {
+    // router.push("/dashboard")
+    redirect("/dashboard");
+  }
   return (
     <div>
-      <LandingPage/>
+      <LandingPage />
     </div>
   );
 }
